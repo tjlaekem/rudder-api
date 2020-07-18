5e3676d7e80a0af16b13e27e4a223e3e56f75705
taylor <taylor.laekeman@gmail.com>
2020-07-18 13:16:33 -0400

    fix lint errors

--- app/graphql/mutations/create_goal.rb
+++ app/graphql/mutations/create_goal.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Mutations
   class CreateGoal < BaseMutation
     argument :text, String, required: true
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Goal
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :text, type: String, default: ""
+  field :text, type: String, default: ''
   field :is_achieved, type: Mongoid::Boolean, default: false
 end
